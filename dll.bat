b4c84f4c8af6f583c3573e2db938d20eceba529c
topet2k12001 <topet2k12001@users.noreply.github.com>
2020-08-04 20:54:01 +0800

    Update dll.bat

--- dll.bat
+++ dll.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJH6F+UcjFBxRTRbMNWi1Zg==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSTk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbAs=
-::ZQ05rAF9IAHYFVzEqQIUBDcUfgGFNXiuA6d8
-::eg0/rx1wNQPfEVWB+kM9LVsJDCCuECuIA7IV+/z+/Yo=
-::fBEirQZwNQPfEVWB+kM9LVsJDACOMHm/AbwP/O3pjw==
-::cRolqwZ3JBvQF1fEqQICLRxdXxCHLnj6IpkwqO70/aqTqkhdU+Myfe8=
-::dhA7uBVwLU+EWFfL9SI=
-::YQ03rBFzNR3SWATE+g0xSA==
-::dhAmsQZ3MwfNWATE4E0gLQ8GR1XQbDvrZg==
-::ZQ0/vhVqMQ3MEVWAtB9wMR5HLA==
-::Zg8zqx1/OA3MEVWAtB9wJlRVLA==
-::dhA7pRFwIByZRRmKu0NQ
-::Zh4grVQjdCuDJGyX8VAjFDpaWAyNMnKGIrof/eX+4f6UnmA4c8MsYYHYmpCBN+4f5UTgZqoJ8kgauZlcTC5IehO/a08EqG1JinOKJcyVvQihTEGF41gMCGZmjnPEzC0ybNZ7pscP32675Eif
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 
 rem Copyright 2020 topet2k12001
